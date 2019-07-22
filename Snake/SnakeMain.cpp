87d842ae51dfaccd2011b4b9105a8edda02b62c3
ry1ynn <ry1ynn_sec@163.com>
2019-07-22 17:13:37 +0800

    用类指针解决上次问题，正式开始存档读档功能的编写

--- Snake/SnakeMain.cpp
+++ Snake/SnakeMain.cpp
@@ -22,26 +22,30 @@ using namespace std;
 
 int main()
 {
+	CSnake* psnake=NULL;//初始化是个好习惯，高老师如是说道
+	CBarrier* pbarrier=NULL;
+	CFood* pfood=NULL;
+
 	// 1. 初始化及欢迎界面
 	GameInit();
 	DrawWelcome();
 
 	// 2. 用户想干嘛？
 	int op = HandleSelect();
-
+	
 	//2.1 若开启新游戏，则各对象有参实例化
 	if (op == 1)
 	{
-		static CSnake snake(UP);//方向为UP
-		static CBarrier barrier(snake.m_SnakeBody, 15);//默认为20个，也可自定义
-		static CFood food(snake.m_SnakeBody, barrier.m_BarrArr);
+		psnake = new CSnake(UP);//方向为UP
+		pbarrier = new CBarrier(psnake->m_SnakeBody, 15);//默认为20个，也可自定义
+		pfood = new CFood(psnake->m_SnakeBody, pbarrier->m_BarrArr);
 	}
 	//2.2 若读档，从文件读取，则各对象无参实例化，以接受数据
 	else if (op == 2)
 	{
-		static CSnake snake;
-		static CBarrier barrier;
-		static CFood food;
+		psnake = new CSnake;
+		pbarrier = new CBarrier;
+		pfood = new CFood;
 		LoadGame();
 	}
 	// 2.3 退出游戏
@@ -62,23 +66,22 @@ int main()
 	while (g_isRunning)
 	{
 		// 4.1 打印游戏信息、食物、障碍物
-		DrawGameInfo(snake.GetSnakeSize(), barrier.GetBarrSize());	//打印分数等信息
-		food.DrawFood();					//打印食物
-		barrier.DrawBarr();//打印障碍物
+		DrawGameInfo(psnake->GetSnakeSize(), pbarrier->GetBarrSize());	//打印分数等信息
+		pfood->DrawFood();					//打印食物
+		pbarrier->DrawBarr();//打印障碍物
 
 		// 4.2 让蛇移动并将其打印
-		snake.ClearSnake();					//清理蛇尾
-		snake.IsEatenFood(food, barrier.m_BarrArr);			//是否吃到食物
-		snake.MoveSnake();					//让蛇跑起来
-		snake.DrawSanke();					//画蛇
+		psnake->ClearSnake();					//清理蛇尾
+		psnake->IsEatenFood(*pfood, pbarrier->m_BarrArr);			//是否吃到食物
+		psnake->MoveSnake();					//让蛇跑起来
+		psnake->DrawSanke();					//画蛇
 
 		// 4.3 判断是否存活
-		if (!snake.IsAlive(barrier.m_BarrArr))				//是否活着
+		if (!psnake->IsAlive(pbarrier->m_BarrArr))				//是否活着
 		{
-			GameOver(snake.GetSnakeSize());
+			GameOver(psnake->GetSnakeSize());
 			break;
 		}
-
 		// 4.4 控制游戏速度
 		Sleep(g_speed);
 	}
